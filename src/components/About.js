55bf738c1eb71a995fcb84ede0c99f587fd26ec6
Craig Omore <craiggomore@gmail.com>
2025-10-22 09:53:54 +0300

    racing time

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,16 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I’m a passionate developer who enjoys building interactive, user-friendly
+        web applications and learning new technologies.
+      </p>
+      <img src={image} alt="I made this" ></img>
+    </div>
+  );
 }
 
 export default About;
